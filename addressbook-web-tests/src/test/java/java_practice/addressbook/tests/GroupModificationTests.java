4215fb58419961a032f56b6943d27f293220252a
ConstantineZolotukhin <fieryserpent.xxi@gmail.com>
2021-12-31 15:23:05 +0300

    Создан GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/java_practice/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,24 @@
+package java_practice.addressbook.tests;
+
+import java_practice.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase {
+
+   @Test
+   public void testGroupModification () {
+      app.getNavigationHelper().gotoGroupPage();
+      app.getGroupHelper().selectGroup();
+      app.getGroupHelper().initGroupModification();
+      app.getGroupHelper().fillGroupForm(new GroupData("First Test Group Updated", "Group Header Updated", "Group Footer Updated"));
+      app.getGroupHelper().submitGroupModification();
+      app.getGroupHelper().returnToGroupPage();
+   }
+
+
+
+
+
+
+
+}
