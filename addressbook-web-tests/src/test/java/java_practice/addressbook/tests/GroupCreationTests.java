9c75b5f8ca9b6847268386f740a352b893de734b
ConstantineZolotukhin <fieryserpent.xxi@gmail.com>
2022-01-12 09:32:46 +0300

    Добавлена параметризация тестовых методов в GroupCreationTests

--- addressbook-web-tests/src/test/java/java_practice/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/java_practice/addressbook/tests/GroupCreationTests.java
@@ -2,20 +2,31 @@ package java_practice.addressbook.tests;
 
 import java_practice.addressbook.model.GroupData;
 import java_practice.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("First Test Group").withHeader("First Group Header").withFooter("First Group Footer")});
+    list.add(new Object[] {new GroupData().withName("Second Test Group").withHeader("Second Group Header").withFooter("Second Group Footer")});
+    list.add(new Object[] {new GroupData().withName("Third Test Group").withHeader("Third Group Header").withFooter("Third Group Footer")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData()
-            .withName("First Test Group")
-            .withFooter("Group Header")
-            .withHeader("Group Footer");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() +1));
     Groups after = app.group().all();
